0f08c8338d0e7db5ae5bafc2aed063f9353b9c38
cauamillersjc <caua.millersantos@gmail.com>
2023-10-23 18:30:18 -0300

    Inicialização do projeto e migrations

---
+++ database/migrations/2023_10_23_202827_create_colaboradores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColaboradoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colaboradores', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('unidade_id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('email');
+
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colaboradores');
+    }
+}
---
+++ database/migrations/2023_10_23_202857_create_cargo_colaborador_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargoColaboradorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargo_colaborador', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cargo_id');
+            $table->unsignedBigInteger('colaborador_id');
+            $table->integer('nota_desempenho');
+
+            $table->foreign('cargo_id')->references('id')->on('cargos');
+            $table->foreign('colaborador_id')->references('id')->on('colaboradores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargo_colaborador');
+    }
+}
